14ba5d18c2bf157e3bb08859729db101c1c51f4d
Nineteen991 <Nineteen99@gmail.com>
2022-10-22 15:26:18 -0700

    changed the product schema

--- back-end/models/Products.js
+++ back-end/models/Products.js
@@ -10,7 +10,12 @@ const ProductSchema = new mongoose.Schema({
     description: {
         type: String,
         required: [true, 'Need product description'],
-        maxlength: [2000, 'Description cannot be more than 2000 characters']
+        maxlength: [200, 'Description cannot be more than 200 characters']
+    },
+    detailed_desc: {
+        type: String,
+        required: [true, 'Need a more detailed product description'],
+        maxlength: [2000, 'Detailed description cannot be more than 2000 characters']
     },
     price: {
         type: Number,
@@ -21,10 +26,19 @@ const ProductSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
-    image: {
-        type: String,
-        required: [true, 'Need product image'],
-        default: '/uploads/ancient-stone-spheres.webp'
+    primary_images: {
+        images: [{
+            type: String,
+            required: [true, 'Need image box images'],
+            default: '/uploads/ancient-stone-spheres.webp'
+        }]
+    },
+    secondary_images: {
+        images: [{
+            url: String,
+            text: String,
+            css_property: String
+        }]
     },
     inventory: {
         type: Number,
--- front-end/src/routes/Store.js
+++ front-end/src/routes/Store.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 
 import { Context } from '../ContextApiData'
 import GameModal from '../components/GameModal'
